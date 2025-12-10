71e9540adbb5f33a44fc02da045044468aca6db7
Lans12345 <olanalans12345@gmail.com>
2025-12-11 05:57:35 +0800

    update

--- lib/screens/pages/contactus_page.dart
+++ lib/screens/pages/contactus_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:phara_driver/utils/colors.dart';
+import 'package:phara_driver/widgets/text_widget.dart';
 
-import '../../utils/colors.dart';
 import '../../widgets/appbar_widget.dart';
 import '../../widgets/drawer_widget.dart';
-import '../../widgets/text_widget.dart';
 
 class ContactusPage extends StatelessWidget {
   const ContactusPage({Key? key}) : super(key: key);
@@ -13,103 +13,118 @@ class ContactusPage extends StatelessWidget {
     return Scaffold(
       drawer: const DrawerWidget(),
       appBar: AppbarWidget('Contact Us'),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/googlelogo.png',
-                  height: 25,
-                  color: grey,
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                TextRegular(
-                    text: 'todacash123@gmail.com', fontSize: 16, color: grey),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Divider(),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/fblogo.png',
-                  height: 25,
-                  color: grey,
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                TextRegular(
-                    text: 'facebook.com/todacash', fontSize: 16, color: grey),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Divider(),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Icon(
-                  Icons.local_phone_outlined,
-                  color: grey,
-                  size: 28,
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                TextRegular(text: '+639457786285', fontSize: 16, color: grey),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Divider(),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Icon(
-                  Icons.pin_drop_outlined,
-                  color: grey,
-                  size: 28,
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                SizedBox(
-                  width: 200,
-                  child: TextRegular(
-                      text: 'Taguig City, Philippines, 1630',
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            color: grey,
+            image: DecorationImage(
+                opacity: 0.5,
+                image: AssetImage('assets/images/dri.png'),
+                fit: BoxFit.cover)),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/googlelogo.png',
+                    height: 25,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  TextRegular(
+                      text: 'todacash123@gmail.com',
                       fontSize: 16,
-                      color: grey),
-                ),
-              ],
-            ),
-          ],
+                      color: Colors.white),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/fblogo.png',
+                    height: 25,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  TextRegular(
+                      text: 'facebook.com/todacash',
+                      fontSize: 16,
+                      color: Colors.white),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.local_phone_outlined,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  TextRegular(
+                      text: '+639457786285', fontSize: 16, color: Colors.white),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.pin_drop_outlined,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: TextRegular(
+                        text: 'San Joaquin, Pasig City, Philippines, 1601',
+                        fontSize: 16,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
